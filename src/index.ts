827e5e338b5de233e358b1f5fd0da923cbe96668
Daniel Sieradski <daniel@self.agency>
2022-11-16 01:34:29 -0500

    wth
    Signed-off-by: Daniel Sieradski <daniel@self.agency>

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,8 @@ export async function main(): Promise<void> {
       const parser = new Parser({
         headers: {
           Accept: '*/*',
-          'User-Agent': 'xh/0.17.0'
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42'
         }
       });
 
